20e88dffd3e163efc8a57c1f570663682224d41d
StephenRedd <redds@reddnet.net>
2013-09-10 14:33:42 -0400

    td3 - fix: signalr initialization

--- TicketDesk-3/TicketDesk.Web/App/services/notifiercontext.js
+++ TicketDesk-3/TicketDesk.Web/App/services/notifiercontext.js
@@ -8,24 +8,20 @@
 
 
         var stopHubs = function () {
-            return Q.when(function() {
                 $.connection.hub.stop();
                 logger.log('SignalR no longer listening for ticket updates', null, system.getModuleId(datacontext), true);
-            });
         };
         var startHubs = function () {
-            return Q.when(function() {
-                var sig = $.connection.ticketDeskHub;
-                $.connection.hub.start();
-                logger.log('SignalR listening for ticket updates', null, system.getModuleId(datacontext), true);
-                sig.client.ticketChanged = function(ticketId) {
-                    logger.log('Server says: ticket ' + ticketId + ' changed', null, 'home', true);
-                    datacontext.refreshTicketById(ticketId);
-                };
-            });
+            var sig = $.connection.ticketDeskHub;
+            $.connection.hub.start();
+            logger.log('SignalR listening for ticket updates', null, system.getModuleId(datacontext), true);
+            sig.client.ticketChanged = function (ticketId) {
+                logger.log('Server says: ticket ' + ticketId + ' changed', null, 'home', true);
+                datacontext.refreshTicketById(ticketId);
+            };
         };
 
-        var activate = function() {
+        var activate = function () {
             var x = datacontext;
         };
 
