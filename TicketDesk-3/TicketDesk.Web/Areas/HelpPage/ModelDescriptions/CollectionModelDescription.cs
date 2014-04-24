e4b0f746744ea85e5518ab903b7b02f9312b0b7c
StephenRedd <redds@reddnet.net>
2014-04-23 23:16:43 -0400

    td3 - updated webapi 2.1 & own components

---
+++ TicketDesk-3/TicketDesk.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TicketDesk.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
