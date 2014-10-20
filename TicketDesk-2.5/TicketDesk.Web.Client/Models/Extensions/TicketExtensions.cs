6853b5c8d5065d4f5b841fe43d4cf4bca9a2a21f
Stephen Redd <redds@reddnet.net>
2014-10-20 00:50:32 -0400

    gimpy version of user display name conversion for ticket center

---
+++ TicketDesk-2.5/TicketDesk.Web.Client/Models/Extensions/TicketExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using TicketDesk.Domain.Model;
+using TicketDesk.Web.Identity.Model;
+
+namespace TicketDesk.Web.Client.Models.Extensions
+{
+    public static class TicketExtensions
+    {
+        public static TicketDeskUser GetAssignedToUser(this Ticket ticket)
+        {
+            return GetUser(ticket.AssignedTo);
+        }
+        public static TicketDeskUser GetCreatedByUser(this Ticket ticket)
+        {
+            return  GetUser(ticket.CreatedBy);
+        }
+        public static TicketDeskUser GetOwnerUser(this Ticket ticket)
+        {
+            return  GetUser(ticket.Owner);
+        }
+        public static TicketDeskUser GetLastUpdatedByUser(this Ticket ticket)
+        {
+            return  GetUser(ticket.LastUpdateBy);
+        }
+        public static TicketDeskUser GetCurrentStatusSetByUser(this Ticket ticket)
+        {
+            return  GetUser(ticket.CurrentStatusSetBy);
+        }
+
+        private static TicketDeskUser GetUser(string userId)
+        {
+            //TODO: this is a lot of query for every ticket, need to implement serious caching
+            var userManager = DependencyResolver.Current.GetService<TicketDeskUserManager>();
+            return userManager.FindById(userId);
+        }
+    }
+}
