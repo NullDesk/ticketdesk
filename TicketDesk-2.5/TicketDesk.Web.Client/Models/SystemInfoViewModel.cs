7cbdba5cb4809fb8563de35b83c89aa3742246b4
Stephen Redd <redds@reddnet.net>
2015-01-05 22:24:14 -0500

    cleanup and attributions

--- TicketDesk-2.5/TicketDesk.Web.Client/Models/SystemInfoViewModel.cs
+++ TicketDesk-2.5/TicketDesk.Web.Client/Models/SystemInfoViewModel.cs
@@ -1,9 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// TicketDesk - Attribution notice
+// Contributor(s):
+//
+//      Stephen Redd (stephen@reddnet.net, http://www.reddnet.net)
+//
+// This file is distributed under the terms of the Microsoft Public 
+// License (Ms-PL). See http://opensource.org/licenses/MS-PL
+// for the complete terms of use. 
+//
+// For any distribution that contains code from this file, this notice of 
+// attribution must remain intact, and a copy of the license must be 
+// provided to the recipient.
+
+using System;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 using TicketDesk.Domain;
 using TicketDesk.Domain.Search.AzureSearch;
 using TicketDesk.IO;
