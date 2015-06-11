17fdaf7eba207e3c6b127f145bac19c33dbe07e1
Stephen Redd <redds@reddnet.net>
2015-06-10 21:07:07 -0400

    fix #27 - redesigned and simplified authentication ui and account management ui

---
+++ TicketDesk-2.5/TicketDesk.Web.Client/Controllers/AccountController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using TicketDesk.Web.Client.Models;
+using TicketDesk.Web.Identity;
+using TicketDesk.Web.Identity.Model;
+
+namespace TicketDesk.Web.Client.Controllers
+{
+    [Authorize]
+    [RoutePrefix("account")]
+    [Route("{action=manage}")]
+    public class AccountController : Controller
+    {
+        private TicketDeskUserManager UserManager { get; set; }
+        private TicketDeskSignInManager SignInManager { get; set; }
+
+        public AccountController(TicketDeskUserManager userManager, TicketDeskSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+
+        [Route("manage")]
+        public ActionResult Manage(AccountMessageId? message)
+        {
+            ViewBag.StatusMessage =
+               message == AccountMessageId.ChangePasswordSuccess ? "Your password has been changed."
+               : message == AccountMessageId.Error ? "An error has occurred."
+               : message == AccountMessageId.ProfileSaveSuccess ? "Your profile has been updated."
+               : "";
+            return View();
+        }
+
+        [Route("change-password")]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [Route("change-password")]
+        [HttpPost]
+        public async Task<ActionResult> ChangePassword(AccountPasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+            if (result.Succeeded)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                if (user != null)
+                {
+                    await SignInAsync(user, isPersistent: false);
+                }
+                return RedirectToAction("Manage", new { Message = AccountMessageId.ChangePasswordSuccess });
+            }
+            AddErrors(result);
+            return View(model);
+        }
+
+        [Route("edit-profile")]
+        public ActionResult EditProfile()
+        {
+            var model = new AccountProfileViewModel {DisplayName = User.Identity.GetUserDisplayName(), Email = User.Identity.GetUserName()};
+            return View(model);
+        }
+
+        [Route("edit-profile")]
+        [HttpPost]
+        public async Task<ActionResult> EditProfile(AccountProfileViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            user.UserName = model.Email;
+            user.Email = model.Email;
+            user.DisplayName = model.DisplayName;
+            var result = await UserManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                AuthenticationManager.SignOut();
+                await SignInManager.SignInAsync(user, false, false);//.SignIn(new AuthenticationProperties { IsPersistent = false }, 
+                //await user.GenerateUserIdentityAsync(UserManager);
+                return RedirectToAction("Manage", new { Message = AccountMessageId.ProfileSaveSuccess });
+            }
+            return View();
+        }
+
+        public enum AccountMessageId
+        {
+            ChangePasswordSuccess,
+            ProfileSaveSuccess,
+            Error
+        }
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        private async Task SignInAsync(TicketDeskUser user, bool isPersistent)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie, DefaultAuthenticationTypes.TwoFactorCookie);
+            AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = isPersistent }, await user.GenerateUserIdentityAsync(UserManager));
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
--- TicketDesk-2.5/TicketDesk.Web.Client/Models/AccountViewModel.cs
+++ TicketDesk-2.5/TicketDesk.Web.Client/Models/AccountViewModel.cs
@@ -11,11 +11,30 @@
 // attribution must remain intact, and a copy of the license must be 
 // provided to the recipient.
 
-    using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace TicketDesk.Web.Client.Models
 {
-    public class UserRegisterViewModel
+    public class AccountPasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class AccountProfileViewModel
     {
         [Required]
         [EmailAddress]
@@ -26,32 +45,5 @@ namespace TicketDesk.Web.Client.Models
         [StringLength(100)]
         [Display(Name = "Display Name")]
         public string DisplayName { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-    }
-
-    public class UserSignInViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
     }
 }
