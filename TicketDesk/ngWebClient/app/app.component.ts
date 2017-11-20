7abcbee1fb0ceb8a3957f80dd85d634aa979220f
Daniel Maida <danielmaida2@gmail.com>
2017-11-19 17:29:00 -0800

    Added ngWebClient to ticketdesk. MVC with Angular This is a new project file with .NET MVC framework, but the website made with the angular2 framework. This was actually made by Joscha, At github https://github.com/JoschaO.

---
+++ TicketDesk/ngWebClient/app/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: `<h1>Hi {{a}}</h1>`,
+})
+export class AppComponent { a = 'Congratulation! you have created your first application using Angular2 with ASP.NET MVC 5'; }
