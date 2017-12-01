d9385eb341c190d8502ebc5253f4508a826049e2
Maya Kate <maya.k.hess@gmail.com>
2017-12-01 14:31:41 -0800

    Forgot the module?

--- TicketDesk/ngWebClient-StandAlone/src/app/app.module.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
-
+import { NotFoundComponent } from './not-found.component';
 import { AppComponent } from './app.component';
+import { FoobarComponent } from './foobar.component';
 
+const appRoutes: Routes = [
+	{ path: '', component: FoobarComponent },
+	{ path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	  NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	  NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
