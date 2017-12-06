4479f383861edb9eb33273607193030f96976089
Daniel Maida <danielmaida2@gmail.com>
2017-12-06 14:51:14 -0800

    Single ticket view component now looks for the url to get the correct ticket id. You can see a ticket by doing localhost:4200/ticket/1111. There is currently only two ticket IDs in the 'mockups'.

--- TicketDesk/ngWebClient-StandAlone/src/app/activity-log/activity-log.component.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/activity-log/activity-log.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SingleTicketService } from '../services/single-ticket.service';
 import { Ticket, Logs, Entry } from '../models/data';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-activity-log',
@@ -8,17 +9,21 @@ import { Ticket, Logs, Entry } from '../models/data';
   styleUrls: ['./activity-log.component.css']
 })
 export class ActivityLogComponent implements OnInit {
-
+  
   ticket_log:[Entry] = null;
-
-
-  constructor(private singleTicketService : SingleTicketService) { }
-
-  ngOnInit() {
-
-  	this.ticket_log = this.singleTicketService.getTicketLog(123456);
-  	console.log('Ticket log: ' + this.ticket_log)
-
+  ticketId: number = null;
+  
+  constructor(private singleTicketService : SingleTicketService, private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.params.subscribe(params => {
+      this.ticketId = params['ticketID'];
+    });
   }
-
+  
+  ngOnInit() {
+    
+    this.ticket_log = this.singleTicketService.getTicketLog(this.ticketId);
+    console.log('Ticket log: ' + this.ticket_log)
+    
+  }
+  
 }
--- TicketDesk/ngWebClient-StandAlone/src/app/services/ticket_db.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/ticket_db.ts
@@ -2,7 +2,7 @@ import { Ticket, Logs } from '../models/data'
 
 export let tickets:[Ticket] = [
 
-    {"ticketId": 123456, "projectId": 1, 
+    {"ticketId": 1111, "projectId": 1, 
     "comment": "Initial Submission", 
     "details": "Two weeks ago, my EVGA GTX 780 Ti of 3.5 years started to malfunction (artifacts, display disruptions, etc.) and shortly after that, finally died. This was especially annoying because it was only half a year after EVGA's warranty expired. So I researched online and bought an EVGA GTX 1080 Ti (which I can still return). This card is overkill for my setup, I do know that, especially for gaming with 1920x1080. However, I intend to buy a second monitor with either a 2k or 4k resolution which should be enough to keep up with this beast.", "title": "My Computer is broken.", 
     "ticketType": "type test", 
@@ -12,7 +12,7 @@ export let tickets:[Ticket] = [
     "tagList": ["tag1", "tag2"]
 },
 
-    {"ticketId": 10, 
+    {"ticketId": 2222, 
     "projectId": 11,
      "comment": "test comment 1",
       "title": "test title 1",
@@ -26,7 +26,7 @@ export let tickets:[Ticket] = [
 ];
 
 export let logs:[Logs] = [
-	{"ticketId": 123456,
+	{"ticketId": 1111,
 	"entries" : [
 	{	"owner" : "Tech #123",
 		"description" : "To be, or not to be: that is the question: Whether ‘tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; No more; and by a sleep to say we end",
@@ -50,7 +50,7 @@ export let logs:[Logs] = [
 	]
 	},
 	
-	{"ticketId": 10,
+	{"ticketId": 2222,
 	"entries" : [
 	{	"owner" : "User",
 		"description" : "Test ticket Submission",
