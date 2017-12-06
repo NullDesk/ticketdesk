0112ec68586316708d5c374e55835f8c5cfa7681
Maya Kate <maya.k.hess@gmail.com>
2017-12-06 14:28:58 -0800

    Add a blank ticket (temporary, deo volente)

--- TicketDesk/ngWebClient-StandAlone/src/app/models/data.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/models/data.ts
@@ -11,3 +11,10 @@ export interface Ticket {
 	owner: string, 
 	tagList: string	
 }
+
+export const BLANK_TICKET: Ticket = {ticketId: -1,
+projectId: -1,
+	comment: '', title: '', details: '', ticketType: '',
+	category: '', subcategory: '', owner: '', tagList: ''
+
+} 
