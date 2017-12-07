e11599218b26b047910b712e5ecda2ce94533ebf
Colin M St. Claire <colin.stclaire@wsu.edu>
2017-12-06 19:20:43 -0800

    cleaned up ugliness

--- TicketDesk/ngWebClient-StandAlone/src/app/services/single-ticket.service.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/single-ticket.service.ts
@@ -1,45 +1,45 @@
 import { Injectable } from '@angular/core';
-import { Ticket , Logs, Entry } from '../models/data';
-import { tickets, logs } from './ticket_db'
+import { Ticket, Logs, Entry } from '../models/data';
+import { tickets, logs } from './ticket_db';
+
 @Injectable()
 export class SingleTicketService {
 
-  constructor() {};
+  constructor() { }
 
-  getTicketDetails(ticketId: number):Ticket{
-    let get_ticket:Ticket = null; // 
+  getTicketDetails(ticketId: number): Ticket {
+    let get_ticket: Ticket = null; //
 
-    for(let ticket of tickets){ // "search" database here to match ticketId
-      if(ticket.ticketId == ticketId){
+    for (let ticket of tickets) { // "search" database here to match ticketId
+      if (ticket.ticketId === ticketId) {
         get_ticket = ticket;
         break;
       }
-    };
-    
-	 return get_ticket; 
-  };
+    }
 
-  getTicketFiles(ticketId: number){
+    return get_ticket;
+  }
 
-  };
+  getTicketFiles(ticketId: number) {
 
-  getTicketLog(ticketId: number):[Entry]{
-    for(let log of logs){
-      console.log("log.ticketId: " + log.ticketId);
-      console.log("ticketId: " + ticketId)
+  }
 
-      if (log.ticketId == ticketId) {
-        console.log("Entered if statement");
-        console.log("entries list: " + log.entries);
+  getTicketLog(ticketId: number): [Entry] {
+    for (let log of logs) {
+      console.log(`log.ticketId: ${log.ticketId}`);
+      console.log(`ticketId: ${ticketId}`);
+
+      if (log.ticketId === ticketId) {
+        console.log('Entered if statement');
+        console.log(`entries list: ${log.entries}`);
         return log.entries;
       }
     }
     return null;
+  }
 
-  };
+  changeTicketSubscription(ticketID: number) {
 
-  changeTicketSubscription(ticketID: number){
-
-  };
+  }
 
 }
