af9ac74a3d6dc37e23d4636830c0ab5eb1722375
Nazar Stelmakh (Ubuntu) <nazar.stelmakh@wsu.edu>
2017-12-06 17:39:44 -0800

    Working on multi ticket veiw to display tabs

--- TicketDesk/ngWebClient-StandAlone/src/app/services/services.module.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/services.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileService } from './file.service';
 import { SingleTicketService } from './single-ticket.service';
+import { MultiTicketService } from './multi-ticket.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers: [FileService, SingleTicketService]
+  providers: [FileService, SingleTicketService, MultiTicketService]
 })
 export class ServicesModule { }
