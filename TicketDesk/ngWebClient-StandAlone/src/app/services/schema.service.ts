a270e1e0bc41fc2d2469f83b92808eae0ac7273c
Maya Kate <maya.k.hess@gmail.com>
2017-12-06 23:05:20 -0800

    Rebuild the stuff I forgot to commit.

--- TicketDesk/ngWebClient-StandAlone/src/app/services/schema.service.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/schema.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
+import { tickets } from './ticket_db';
+
 
 @Injectable()
 export class SchemaService {
+	private types: string[];
+	private categoryTree;
+
   getCategoryTree() {
-	return {
-		"cat": ["hairball", "tail flippy", "sudden death"],
-		"dog": ["congenital stupidity", "shedding", "aggression"],
-		"frog": ["ew slimy"]
-	};
+	return this.categoryTree 
   	
   }
 
 	getTicketTypes() {
-  return ["Problem", "Big Problem", "Explosion"];
+  return this.types 
   }
   
-	constructor() { }
+	constructor() {
+		let categories = Array.from(new Set(tickets.map((tkt) => tkt["category"])));
+console.log(categories);
+this.types = Array.from( new Set(tickets.map((tkt) => tkt["ticketType"])));
+	this.categoryTree = {};
+		for (let cat of categories) {
+		let subcats = Array.from( new Set(
+		 tickets.filter((x) => x["category"] == cat).map((x) => x["subcategory"])
+		));
+			this.categoryTree[cat] = subcats;
+		}
+		console.log(this.categoryTree);
+	}
+	
 
 }
--- TicketDesk/ngWebClient-StandAlone/src/app/ticket-detail-editor/ticket-detail-editor.component.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/ticket-detail-editor/ticket-detail-editor.component.ts
@@ -1,6 +1,7 @@
 import { Input, Inject, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SchemaService } from '../services/schema.service';
 import { SubmitTicketService } from '../services/submit-ticket.service';
 import {Ticket, BLANK_TICKET} from '../models/data';
 import {AttachFileComponent } from '../attach-file/attach-file.component';
@@ -8,18 +9,21 @@ import {AttachFileComponent } from '../attach-file/attach-file.component';
   selector: 'app-ticket-detail-editor',
   templateUrl: './ticket-detail-editor.component.html',
   styleUrls: ['./ticket-detail-editor.component.css'],
-  providers: [SubmitTicketService]
+  providers: [SubmitTicketService, SchemaService]
 })
 export class TicketDetailEditorComponent implements OnInit {
   form: FormGroup;
 	displayedSubcategories: string[] = ["Select a category"];
-	subcategories = {
-		"cat": ["hairball", "tail flippy", "sudden death"],
-		"dog": ["congenital stupidity", "shedding", "aggression"],
-		"frog": ["ew slimy"]
-	};
-	categories = Object.keys(this.subcategories);
-  constructor(@Inject(FormBuilder) fb: FormBuilder, private sts: SubmitTicketService, private router: Router) {
+	subcategories: Object = {};
+	ticketTypes: string[];
+	categories: string[];
+  constructor(@Inject(FormBuilder) fb: FormBuilder, 
+	  private sts: SubmitTicketService, 
+	  private router: Router,
+  private schema: SchemaService) {
+	  this.subcategories = schema.getCategoryTree();
+	this.categories = Object.keys(this.subcategories);
+	  this.ticketTypes = schema.getTicketTypes();
 	  this.form = fb.group(
 	    BLANK_TICKET);
 	  this.form.get("category").valueChanges.subscribe(
