16d538c222003a1f5db504dca7a32548f2239c8f
Colin M St. Claire <colin.stclaire@wsu.edu>
2017-12-06 23:27:37 -0800

    Added dynamic ticket button options

--- TicketDesk/ngWebClient-StandAlone/src/app/services/single-ticket.service.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/single-ticket.service.ts
@@ -16,7 +16,6 @@ export class SingleTicketService {
         break;
       }
     }
-
     return get_ticket;
   }
 
@@ -42,4 +41,15 @@ export class SingleTicketService {
 
   }
 
+  getAvailableTicketActions(ticketId: number): Array<String> {
+    // available actions: AddComment (all users), Assign (staff/admin), Close, EditTicketInfo (regular), ForceClose (regular), GiveUp, 
+    // ModifyAttachments (all users), Pass, RequestMoreInfo, ReOpen, Resolve, SupplyMoreInfo, TakeOver (staff/admin)
+    const ticketActions = {
+      1111: ['AddComment', 'ModifyAttachments', 'EditTicketInfo', 'ForceClose'],
+      2222: ['AddComment', 'ModifyAttachments', 'Assign', 'TakeOver'],
+      3333: ['AddComment', 'ModifyAttachments', 'Assign', 'TakeOver']
+    }
+    const actions: Array<String> = ticketActions[ticketId]
+    return actions;
+  }
 }
