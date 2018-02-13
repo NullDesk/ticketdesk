080375c36c61042a4a9c262d931358a1cebd8b4b
Joscha Oswald <joschaoswald@gmail.com>
2018-02-12 16:57:06 -0800

    Implemented ngModal with .state function instead. So there doesn't have to be 2 lists.

--- TicketDesk/ngWebClient-StandAlone/src/app/ticket-list/ticket-list.component.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/ticket-list/ticket-list.component.ts
@@ -8,46 +8,24 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./ticket-list.component.css']
 })
 
-
 export class TicketListComponent implements OnInit {
   //This will become input
   private headingsList: string[] = ['Title', 'Status', 'Priority', 'Owner', 'Assigned', 'Category', 'Created Date'];
-  private displayList: { "ticket": Ticket, "isChecked": boolean }[]
   @Input() ticketListResults: { "ticketList": Ticket[], "maxPages": number };
   @Input() columns: string[];
-  selected = new Set();
-  isSelected: Map<number, boolean> = new Map;
   currentPage: number;
-  //TODO RENAME TICKETLIST THROUGHOUT
   ngOnInit() {
-    this.displayList = this.ticketListResults.ticketList.map(
-      function (x) { return { "ticket": x, "isChecked": false } }
-    )
+    //State is created from ngModal in Angular
+    this.ticketListResults.ticketList.forEach(x => x.state = false)
     this.currentPage = 1;
-    for (let ticket of this.ticketListResults.ticketList) {
-      this.isSelected.set(ticket.ticketId, false);
-    }
   }
   isAllChecked() {
-    return this.displayList.every(x => x.isChecked)
-  }
-  isChecked(ticket: Ticket) {
-    return this.isSelected.get(ticket.ticketId)
+    return this.ticketListResults.ticketList.every(x => x.state)
   }
   selectAll(ev) {
-    this.displayList.forEach(x => { x.isChecked = ev.target.checked })
+    this.ticketListResults.ticketList.forEach(x => { x.state = ev.target.checked })
   }
-
-  checkboxSelect(ticket: Ticket) {
-    //optimize this with another selection array
-    if (this.isSelected.get(ticket.ticketId)) {
-      this.selected.delete(ticket)
-    } else {
-      this.selected.add(ticket)
-    }
-    this.isSelected.set(ticket.ticketId,
-      !this.isSelected.get(ticket.ticketId))
+  getSelected() {
+    return this.ticketListResults.ticketList.filter( x => x.state)
   }
-
-
 }
