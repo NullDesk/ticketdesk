049ff0cd35c638a3828532bd90897dfd8d5ead29
Joscha Oswald <joschaoswald@gmail.com>
2018-02-26 13:55:35 -0800

    Fixed tslint errors

--- TicketDesk/ngWebClient-StandAlone/src/app/ticket-list/ticket-list.component.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/ticket-list/ticket-list.component.ts
@@ -9,23 +9,23 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class TicketListComponent implements OnInit {
-  //This will become input
+  // This will become input
   private headingsList: string[] = ['Title', 'Status', 'Priority', 'Owner', 'Assigned', 'Category', 'Created Date'];
-  @Input() ticketListResults: { "ticketList": Ticket[], "maxPages": number };
+  @Input() ticketListResults: { 'ticketList': Ticket[], 'maxPages': number };
   @Input() columns: string[];
   currentPage: number;
   ngOnInit() {
-    //State is created from ngModal in Angular
-    this.ticketListResults.ticketList.forEach(x => x.state = false)
+    // State is created from ngModal in Angular
+    this.ticketListResults.ticketList.forEach(x => x.state = false);
     this.currentPage = 1;
   }
   isAllChecked() {
-    return this.ticketListResults.ticketList.every(x => x.state)
+    return this.ticketListResults.ticketList.every(x => x.state);
   }
   selectAll(ev) {
-    this.ticketListResults.ticketList.forEach(x => { x.state = ev.target.checked })
+    this.ticketListResults.ticketList.forEach(x => { x.state = ev.target.checked; });
   }
   getSelected() {
-    return this.ticketListResults.ticketList.filter( x => x.state)
+    return this.ticketListResults.ticketList.filter( x => x.state);
   }
 }
