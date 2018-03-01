fcad402909d5aedecb759089f125bd448bcefdac
Colin M St. Claire <colinst.claire@gmail.com>
2018-02-28 16:40:21 -0800

    removed comments

--- TicketDesk/ngWebClient-StandAlone/src/app/services/single-ticket.service.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/single-ticket.service.ts
@@ -2,25 +2,14 @@ import { Injectable } from '@angular/core';
 import { Ticket, Logs, Entry } from '../models/data';
 import { tickets, logs } from './ticket_db';
 
-/**
- * DESIGN THOUGHTS:
- * Since this class will only deal with single tickets, I think we should inject
- * a class that actually makes calls to API (api.service.ts?), and assign a class
- * member var to that ticket so all functions have access to it and returning
- * various ticket properties will be handled simply
- */
-
-
 @Injectable()
 export class SingleTicketService {
 
-  // probably need an injection from class that makes calls to API
   constructor() { 
-    // should we get ticket also as injection? 
   }
 
   getTicketDetails(ticketId: number): Ticket {
-    let get_ticket: Ticket = null; //
+    let get_ticket: Ticket = null; 
 
     for (let ticket of tickets) { // "search" database here to match ticketId
       if (ticket.ticketId == ticketId) {
@@ -32,7 +21,7 @@ export class SingleTicketService {
   }
 
   getOwner(ticketId: number) {
-    // const ticket = 
+    
   }
 
   getTicketFiles(ticketId: number) {
@@ -58,8 +47,6 @@ export class SingleTicketService {
   }
 
   getAvailableTicketActions(ticketId: number): Array<String> {
-    // available actions: AddComment (all users), Assign (staff/admin), Close, EditTicketInfo (regular), ForceClose (regular), GiveUp, 
-    // ModifyAttachments (all users), Pass, RequestMoreInfo, ReOpen, Resolve, SupplyMoreInfo, TakeOver (staff/admin)
     const ticketActions = {
       1111: ['AddComment', 'ModifyAttachments', 'EditTicketInfo', 'ForceClose'],
       2222: ['AddComment', 'ModifyAttachments', 'Assign', 'TakeOver'],
