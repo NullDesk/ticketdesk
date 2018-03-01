889784131eaa7584b11ab8d2341190bc10bb0c40
Daniel Maida <danielmaida2@gmail.com>
2018-03-01 03:07:05 -0800

    Added an Owin Startup class to the Web API that actually gets classes. Now we need to figure out how to port over TicketDesk.Web.Client Startup class.

---
+++ TicketDesk/ngWebClientAPI/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(ngWebClientAPI.Startup))]
+
+namespace ngWebClientAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            //HttpConfiguration httpConfiguration = new HttpConfiguration();
+            //WebApiConfig.Register(httpConfiguration);
+            //app.UseWebApi(httpConfiguration);
+
+        }
+    }
+}
