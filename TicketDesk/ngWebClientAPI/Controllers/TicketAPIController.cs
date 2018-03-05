0c8c188580eac5ea68e61e785a53b10845923b29
Kevin Harris <k.michael7.harris8@gmail.com>
2018-03-04 19:25:51 -0800

    Added data class and conversion for GET requests to pass back to the front end for displaying

--- TicketDesk/ngWebClientAPI/Controllers/TicketAPIController.cs
+++ TicketDesk/ngWebClientAPI/Controllers/TicketAPIController.cs
@@ -4,32 +4,53 @@ using System.Web.Http;
 using TicketDesk.Domain;
 using TicketDesk.Domain.Model;
 using Newtonsoft.Json;
+using System.Web.Mvc;
+using System.Net;
 
 namespace ngWebClientAPI.Controllers
 {
-    [RoutePrefix("api/ticket")]
+    [System.Web.Http.RoutePrefix("api/ticket")]
     public class TicketAPIController : ApiController
     {
         private TicketController ticketController = new TicketController(new TdDomainContext());
 
-        [HttpGet]
-        [Route("")]
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("")]
         public async Task<string> getAllTickets()
         {
-            var model = await ticketController.GetTicketList(); //returns list of all tickets
+            try
+            {
+                var model = await ticketController.GetTicketList(); //returns list of all tickets
+                return JsonConvert.SerializeObject(model);
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("{ticketId}")]
+        public async Task<string> getSingleTicket(int ticketId)
+        {
+            HttpStatusCodeResult result;
+            /*try
+            {
+                Ticket model = await ticketController.getTicket(ticketId);
+                result = new HttpStatusCodeResult(HttpStatusCode.OK, JsonConvert.SerializeObject(model));
+            }
+            catch(Exception ex)
+            {
+                result = new HttpStatusCodeResult(HttpStatusCode.BadRequest, ex.Message);
+            }*/
+            Ticket model = await ticketController.getTicket(ticketId);
+            //result = new HttpStatusCodeResult(HttpStatusCode.OK, JsonConvert.SerializeObject(model));
+            //return result;
             return JsonConvert.SerializeObject(model);
         }
 
-        [HttpGet]
-        [Route("{ticketId}")]
-        public async Task<Ticket> getSingleTicket(int ticketId)
-        {
-            Ticket model = await ticketController.getTicket(ticketId);
-            return model;
-        }
-
-        [HttpPost]
-        [Route("")]
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("")]
         public async Task<Boolean> createTicket([FromBody]Ticket value)
         {
             bool status = await ticketController.CreateTicketAsync(value);
---
+++ TicketDesk/ngWebClientAPI/Models/APITicketConversion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TicketDesk.Domain.Model;
+using Newtonsoft.Json;
+
+namespace ngWebClientAPI.Models
+{
+    public class APITicketConversion
+    {
+        public string ConvertGETTicket(Ticket ticket)
+        {
+            FrontEndTicket FETicket = new FrontEndTicket();
+            FETicket.ticketId = ticket.TicketId;
+            FETicket.projectId = ticket.ProjectId;
+            FETicket.comment = ticket.Details;
+            FETicket.priority = ticket.Priority;
+            FETicket.ticketType = ticket.TicketType;
+            FETicket.category = ticket.Category;
+            FETicket.subcategory = null; //no subcategory in TicketDesk db, might want to add?
+            FETicket.owner = ticket.Owner;
+            FETicket.assignedTo = ticket.AssignedTo;
+            FETicket.status = ticket.TicketStatus;
+            FETicket.tagList = ticket.TicketTags.ToString(); //should work to put tags in string representation of list
+            FETicket.createdDate = ticket.CreatedDate.ToString();
+            return JsonConvert.SerializeObject(FETicket);
+        }
+    }
+
+    public class FrontEndTicket
+    {
+        public int ticketId { get; set; }
+        public int projectId { get; set; }
+        public string comment { get; set; }
+        public string details { get; set; }
+        public string priority { get; set; }
+        public string ticketType { get; set; }
+        public string category { get; set; }
+        public string subcategory { get; set; }
+        public string owner { get; set; }
+        public string assignedTo { get; set; }
+        public TicketStatus status { get; set; }
+        public string tagList { get; set; }
+        public string createdDate { get; set; }
+    }
+}
