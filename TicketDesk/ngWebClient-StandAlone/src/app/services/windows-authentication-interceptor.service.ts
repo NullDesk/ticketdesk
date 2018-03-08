5c98c00091532c33001565a534adeb6e8556746b
Maya Kate <maya.k.hess@gmail.com>
2018-03-07 16:12:07 -0800

    Beginning of interceptor

---
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/windows-authentication-interceptor.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpInterceptor,
+  HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class WindowsAuthenticationInterceptorService {
+
+  constructor() { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      withCredentials: true
+    });
+    return next.handle(req);
+  }
+}
