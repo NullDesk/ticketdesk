65d10070ab988c88bce1c97ede1ba7920b65d725
Joscha Oswald <joschaoswald@gmail.com>
2018-03-07 19:50:06 -0800

    Created the component

---
+++ TicketDesk/ngWebClient-StandAlone/src/app/search-results-view/search-results-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-results-view',
+  templateUrl: './search-results-view.component.html',
+  styleUrls: ['./search-results-view.component.css']
+})
+export class SearchResultsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
