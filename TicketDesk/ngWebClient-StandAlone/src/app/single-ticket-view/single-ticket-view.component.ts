12ffaec41d2e3642deb768ed2f9f7b88a58dcea0
Maya Kate <maya.k.hess@gmail.com>
2018-03-10 11:57:03 -0800

    Correct subscription

--- TicketDesk/ngWebClient-StandAlone/src/app/single-ticket-view/single-ticket-view.component.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/single-ticket-view/single-ticket-view.component.ts
@@ -27,13 +27,8 @@ export class SingleTicketViewComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.singleTicketService.getAvailableTicketActions(this.ticketId).subscribe(response => {
-      // console.warn("response", response['actionPermissions']);
-      // this.ticketActionPermissions = response['actionPermissions']
-      // console.warn("in the subscription function", this.ticketActionPermissions);
-   //  });
-    this.ticketActionPermissions = 31161;
-    console.warn('just on init', this.ticketActionPermissions);
+    this.singleTicketService.getAvailableTicketActions(this.ticketId).subscribe(response => this.ticketActionPermissions = response['actionPermissions']);
+    console.log(this.ticketActionPermissions);
     this.ticket =
       this.singleTicketService.getTicketDetails(this.ticketId);
   }
