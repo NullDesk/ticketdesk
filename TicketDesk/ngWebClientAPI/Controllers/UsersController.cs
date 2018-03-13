fd277af0817e0fc7e5ca4d3a4b98c96ee2d29308
Nazar Stelmakh <nazar7700@gmail.com>
2018-03-12 20:30:03 -0700

    Adding endpoints and working on Windows Authentication

---
+++ TicketDesk/ngWebClientAPI/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ngWebClientAPI.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/users")]
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        [Route("userId")]
+        public string userID()
+        {
+            var userId = System.Web.HttpContext.Current.User.Identity.Name.ToLower().Replace(@"clarkpud\", string.Empty);
+
+            return userId;
+        }
+
+    }
+}
