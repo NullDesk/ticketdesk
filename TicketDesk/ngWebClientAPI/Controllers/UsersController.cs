47924baee66c2529742543eb3b1fbb827fbf419e
Nazar Stelmakh <nazar7700@gmail.com>
2018-03-24 13:55:01 -0700

    Finished endpoint for the user object. Able to return FirstName, LastName, Phone, Email, and list of groups.

--- TicketDesk/ngWebClientAPI/Controllers/UsersController.cs
+++ TicketDesk/ngWebClientAPI/Controllers/UsersController.cs
@@ -4,20 +4,38 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using EmployeeInformationManager;
+using ngWebClientAPI.Models;
+using System.Diagnostics;
 
 namespace ngWebClientAPI.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [RoutePrefix("api/users")]
     public class UsersController : ApiController
     {
         [HttpGet]
         [Route("userId")]
-        public string userID()
+        public CPUUser userID()
         {
-            var userId = System.Web.HttpContext.Current.User.Identity.Name.ToLower().Replace(@"clarkpud\", string.Empty);
+            var employeeManager = new EmployeeInformationManager.EmployeeManager();
+            //var userId = System.Web.HttpContext.Current.User.Identity.Name.ToLower().Replace(@"clarkpud\", string.Empty);
+            var userId = "NStelmakh";
+            var employee = employeeManager.GetADUserByLogin(userId);
+            CPUUser user = new CPUUser();
+            user.FirstName = employee.FirstName;
+            user.LastName = employee.LastName;
+            user.Phone = employee.Phone;
+            user.Email = employee.Email;
+            var mygroup = employee.Groups;
+            user.Groups = new List<string>();
 
-            return userId;
+            foreach (var group in mygroup)
+            {
+                user.Groups.Add(group.Name);
+            }
+
+            return user;
         }
 
     }
---
+++ TicketDesk/ngWebClientAPI/Models/CPUUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ngWebClientAPI.Models
+{
+    public class CPUUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public List<string> Groups { get; set; }
+    }
+}
