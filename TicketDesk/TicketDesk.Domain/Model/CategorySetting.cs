9ae213c4474751f24a855526bf543268cc84a022
Kevin Harris <k.michael7.harris8@gmail.com>
2018-03-24 15:46:07 -0700

    Adding new table to hold categories and subcategories

---
+++ TicketDesk/TicketDesk.Domain/Model/CategorySetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketDesk.Domain.Model
+{
+    public class CategorySetting
+    {
+        public string SubCategory { get; set; }
+        public string Category { get; set; }
+    }
+}
