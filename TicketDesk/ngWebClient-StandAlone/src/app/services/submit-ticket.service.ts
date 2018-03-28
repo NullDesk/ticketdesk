923420e18c2cd0cf428958548341aebbeb5438ea
Maya Kate <maya.k.hess@gmail.com>
2018-03-28 14:59:00 -0700

    Beginning to refactor ticket submission

--- TicketDesk/ngWebClient-StandAlone/src/app/services/submit-ticket.service.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/submit-ticket.service.ts
@@ -1,7 +1,6 @@
 import {Ticket} from '../models/data';
 import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
-
 @Injectable()
 export class SubmitTicketService {
 
@@ -9,16 +8,9 @@ export class SubmitTicketService {
   }
 
   submitTicket(tkt: Ticket) {
-  console.log('ticket submitted:');
   console.log(tkt);
-        // this is where the http stuff goes
-    // then we get to pretend we got the id back from the response
-
-    console.log('POST');
-    const url = `http://localhost:50067/api/tickets`;
-    this.http.post(url, tkt).subscribe(res => console.log(res.json()));
-
-    this.http.get(url + '/1').subscribe(res => console.log(res.text()));
-    return 1111;
+  console.log('POST');
+  const url = `http://localhost:50067/api/tickets`;
+  return this.http.post(url, tkt)
   }
 }
