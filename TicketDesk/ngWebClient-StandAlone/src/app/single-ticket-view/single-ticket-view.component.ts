14307b026103d310ad6abd325ba4e5c37f49194b
Maya Kate <maya.k.hess@gmail.com>
2018-03-28 17:27:15 -0700

    Fix the mystery code appearance

--- TicketDesk/ngWebClient-StandAlone/src/app/single-ticket-view/single-ticket-view.component.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/single-ticket-view/single-ticket-view.component.ts
@@ -27,13 +27,10 @@ export class SingleTicketViewComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.singleTicketService.getAvailableTicketActions(this.ticketId).subscribe(response => {
-      // console.warn("response", response['actionPermissions']);
-      // this.ticketActionPermissions = response['actionPermissions']
-      // console.warn("in the subscription function", this.ticketActionPermissions);
-   //  });
-    this.ticketActionPermissions = 31161;
-    console.warn('just on init', this.ticketActionPermissions);
+    this.singleTicketService.getAvailableTicketActions(this.ticketId).subscribe(response => {
+      console.warn("response", response['actionPermissions']);
+      this.ticketActionPermissions = response['actionPermissions']
+    });
     this.ticket =
       this.singleTicketService.getTicketDetails(this.ticketId);
   }
