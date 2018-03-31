a1f9dbf55eb38bf38fc3cbfe285f5694ccd15237
Maya Kate <maya.k.hess@gmail.com>
2018-03-31 10:55:57 -0700

    Catch API up to dev branch

--- TicketDesk/ngWebClientAPI/App_Start/WebApiConfig.cs
+++ TicketDesk/ngWebClientAPI/App_Start/WebApiConfig.cs
@@ -25,7 +25,7 @@ namespace ngWebClientAPI
                 defaults: new { id = RouteParameter.Optional }
             );
             /*Enable any HTTP request for any controller from the ngWebClient front end.*/
-            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*") { SupportsCredentials = true };
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
             config.EnableCors(cors);
 
            // config.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(DependencyInjectionCoreSetup._Container);
