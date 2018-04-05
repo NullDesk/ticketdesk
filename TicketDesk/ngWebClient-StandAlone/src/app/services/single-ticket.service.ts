f9cf0270ae79497345c546d3833c2926aca15f74
Colin M St. Claire <colinst.claire@gmail.com>
2018-04-04 18:27:54 -0700

    added error handling skeleton

--- TicketDesk/ngWebClient-StandAlone/src/app/services/single-ticket.service.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/single-ticket.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { TicketActionEnum } from '../models/ticket-actions.constants';
 import { Ticket } from '../models/ticket';
 import { Entry } from '../models/entry';
+import { catchError, retry } from 'rxjs/operators';
 import * as settings from '../app-settings';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 
 interface TicketPermissions {
   ticketPermissions: number;
@@ -24,7 +26,8 @@ export class SingleTicketService {
   }
 
   getTicketDetails(ticketId: number): Observable<any> {
-    return this.http.get(settings.ticketDetailsURL + ticketId.toString());
+    return this.http.get(settings.ticketDetailsURL + ticketId.toString())
+      .pipe(catchError(this.handleError));
   }
 
   getOwner(ticketId: number) {
@@ -59,5 +62,16 @@ export class SingleTicketService {
     return this.http.get(
       settings.getValidActionsURL + ticketId.toString()
     );
+  }
+
+  private handleError(error: HttpErrorResponse): ErrorObservable {
+    if (error.error instanceof ErrorEvent) {
+      // ... this is a client side error, handle it!
+      console.error(`client error occurred: ${error.error.message}`)
+    } else {
+      // ... this is a server error!
+      console.error(`server error occurred, status code ${error.status}`)
+    }
+    return new ErrorObservable('Experiencing some issues, we are sorry');
   }
 }
