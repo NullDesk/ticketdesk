d470207647110889c22f6434b69ae89d43831ae4
Maya Kate <maya.k.hess@gmail.com>
2018-04-04 18:18:53 -0700

    Mark response in console

--- TicketDesk/ngWebClient-StandAlone/src/app/ticket-detail-editor/ticket-detail-editor.component.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/ticket-detail-editor/ticket-detail-editor.component.ts
@@ -43,6 +43,7 @@ export class TicketDetailEditorComponent implements OnInit {
     // get back the ID
     this.buttonText = 'Please wait...';
     this.sts.submitTicket(this.form.value).subscribe( res => {
+      console.warn("THIS IS WHAT WE GOT BACK");
       if (res['ticketID']) {
         this.attachFileComponent.addFile();
         this.router.navigate(['/ticket/' + res['ticketID'].toString()]);
