1cafa00dd83185338149b4450820a3fd7865644e
Colin M St. Claire <colinst.claire@gmail.com>
2018-04-07 13:33:12 -0700

    injected `HttpClient` into `ad-contact-service.ts`

--- TicketDesk/ngWebClient-StandAlone/src/app/services/ad-contact.service.ts
+++ TicketDesk/ngWebClient-StandAlone/src/app/services/ad-contact.service.ts
@@ -11,9 +11,25 @@ import * as settings from '../app-settings';
 @Injectable()
 export class AdContactService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getContactCardInfo(userID: string): Observable<UserDetails> {
-    return Observable.of({firstName: 'Juan', lastName: 'Perez', phoneNumber: '(503)555-1515', email: 'jjpe@wherever.com', userId: userID});
+    return Observable.of({
+      firstName: '<First Name>', 
+      lastName: '<Last Name>', 
+      phoneNumber: '<Phone Number>', 
+      email: 'stillneed@activedirectory.com', 
+      userId: userID});
+  }
+
+  private handleError(error: HttpErrorResponse): ErrorObservable {
+    if (error.error instanceof ErrorEvent) {
+      // ... this is a client side error, handle it!
+      console.error(`client error occurred: ${error.error.message}`);
+    } else {
+      // ... this is a server error!
+      console.error(`server error occurred, status code ${error.status}`);
+    }
+    return new ErrorObservable('Experiencing some issues, we are sorry');
   }
 }
