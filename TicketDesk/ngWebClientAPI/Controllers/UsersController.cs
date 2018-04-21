49accd56adb49c0b2be9ee6c518b221fc562d90f
Nazar Stelmakh <nazar7700@gmail.com>
2018-04-21 11:24:47 -0700

    Working on retrieving permissions. Uncommented authorized part. Pushing with MangagerEmployee Nuget package installed. Will need to run on VM if using this branch

--- TicketDesk/ngWebClientAPI/Controllers/UsersController.cs
+++ TicketDesk/ngWebClientAPI/Controllers/UsersController.cs
@@ -4,29 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-//using EmployeeInformationManager;
+using EmployeeInformationManager;
 using ngWebClientAPI.Models;
 using System.Diagnostics;
  namespace ngWebClientAPI.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [RoutePrefix("api/users")]
     public class UsersController : ApiController
     {
         [HttpGet]
-        [Route("userId")]
-        public int userID()
+        [Route("")]
+        public CPUUser userID()
         {
-            /* var employeeManager = new EmployeeInformationManager.EmployeeManager();
-            //var userId = System.Web.HttpContext.Current.User.Identity.Name.ToLower().Replace(@"clarkpud\", string.Empty);
-            var userId = "NStelmakh";
+            var employeeManager = new EmployeeInformationManager.EmployeeManager();
+            var userId = System.Web.HttpContext.Current.User.Identity.Name.ToLower().Replace(@"clarkpud\", string.Empty);
             var employee = employeeManager.GetADUserByLogin(userId);
             CPUUser user = new CPUUser();
             user.FirstName = employee.FirstName;
             user.LastName = employee.LastName;
             user.Phone = employee.Phone;
             user.Email = employee.Email;
-            user.ID = employee.Id;
+            user.ID = employee.Id.ToString();
             var mygroup = employee.Groups;
             user.Groups = new List<string>();
 
@@ -35,7 +34,18 @@ using System.Diagnostics;
                 user.Groups.Add(group.Name);
             }
             
-            return user;*/
+            return user;
+        }
+        [HttpGet]
+        [Route("permissions")]
+        public int GetPermission()
+        {
+            var employeeManager = new EmployeeInformationManager.EmployeeManager();
+            var userId = System.Web.HttpContext.Current.User.Identity.Name.ToLower().Replace(@"clarkpud\", string.Empty);
+            var user = employeeManager.GetADUserByLogin(userId);
+            var permission = user.Groups;
+
+
             return 0;
         }
 
